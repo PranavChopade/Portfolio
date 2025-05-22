764ea0cd22005df96e06adc95b9d80d527fa7519
Pranav Chopade <pranavchopade129@gmail.com>
2025-05-22 11:57:43 +0530

    Added separator Logic

--- Frontend/src/components/Separator.jsx
+++ Frontend/src/components/Separator.jsx
@@ -1,13 +1,22 @@
+const Separator = ({ currentSectionId }) => {
+  const handleScroll = () => {
+    const currentSection = document.getElementById(currentSectionId);
+    if (currentSection && currentSection.nextElementSibling) {
+      currentSection.nextElementSibling.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-const Separator = () => {
   return (
-    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex flex-col items-center" >
-      <span className="animate-bounce text-cyan-400 text-3xl mb-1">
+    <div className="scroll-smooth absolute bottom-4 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
+      <button
+        onClick={handleScroll}
+        className="animate-bounce text-cyan-400 text-3xl mb-1 cursor-pointer"
+      >
         ↓
-      </span>
+      </button>
       <div className="w-36 h-[2px] bg-gradient-to-r from-cyan-400 to-purple-500 opacity-70 animate-fade-in" />
     </div>
-  )
-}
+  );
+};
 
 export default Separator;
