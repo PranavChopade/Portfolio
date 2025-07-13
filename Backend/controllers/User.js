5ef0d1809bc48940f62e70945b207a339e78f5e8
Pranav Chopade <pranavchopade129@gmail.com>
2025-07-13 11:51:08 +0530

    removed unnecessary code

--- Backend/controllers/User.js
+++ Backend/controllers/User.js
@@ -1,28 +1,28 @@
 import User from '../Model/UserModel.js'
 
-export const readDB = async (req, res) => {
-  try {
-    const existingUsers = await User.find();
+// export const readDB = async (req, res) => {
+//   try {
+//     const existingUsers = await User.find();
 
-    if (existingUsers.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: "Users Not Found"
-      });
-    }
+//     if (existingUsers.length === 0) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Users Not Found"
+//       });
+//     }
 
-    res.status(200).json({
-      success: true,
-      users: existingUsers
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Error retrieving users",
-      error: error.message
-    });
-  }
-};
+//     res.status(200).json({
+//       success: true,
+//       users: existingUsers
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: "Error retrieving users",
+//       error: error.message
+//     });
+//   }
+// };
 
 
 export const getUserMessage = async (req, res) => {
@@ -42,48 +42,46 @@ export const getUserMessage = async (req, res) => {
   })
 }
 
-export const deleteUser = async (req, res) => {
-  const { id } = req.params
-  const deletedUser = await User.findByIdAndDelete(id)
+// export const deleteUser = async (req, res) => {
+//   const { id } = req.params
+//   const deletedUser = await User.findByIdAndDelete(id)
 
-  if (!deletedUser) {
-    return res.status(401).json({
-      success: false,
-      message: "unable to delete user"
-    })
-  }
+//   if (!deletedUser) {
+//     return res.status(401).json({
+//       success: false,
+//       message: "unable to delete user"
+//     })
+//   }
 
-  res.status(200).json({
-    success: true,
-    message: "user has been deleted successfully",
-    deletedUser
-  })
-}
+//   res.status(200).json({
+//     success: true,
+//     message: "user has been deleted successfully",
+//     deletedUser
+//   })
+// }
 
+// export const updateUser = async (req, res) => {
+//   try {
+//     const { id } = req.params
+//     const { name } = req.body;
+//     const updatedUser = await User.findByIdAndUpdate(id, { name }, { new: true })
 
-
-export const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params
-    const { name } = req.body;
-    const updatedUser = await User.findByIdAndUpdate(id, { name }, { new: true })
-
-    if (!updatedUser) {
-      res.status(404).json({
-        success: false,
-        message: "no user found with the provided id"
-      })
-    }
-    res.status(200).json({
-      success: true,
-      message: "user updated successfully",
-      user: updatedUser
-    })
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "data updation failed",
-      error: error.message
-    })
-  }
-}
+//     if (!updatedUser) {
+//       res.status(404).json({
+//         success: false,
+//         message: "no user found with the provided id"
+//       })
+//     }
+//     res.status(200).json({
+//       success: true,
+//       message: "user updated successfully",
+//       user: updatedUser
+//     })
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: "data updation failed",
+//       error: error.message
+//     })
+//   }
+// }
