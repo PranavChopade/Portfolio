ddd0628755614d8b7597c4d5a574b3e4668a9999
Pranav Chopade <pranavchopade129@gmail.com>
2025-07-13 12:58:56 +0530

    remove unused user-related routes

--- Backend/Routes/route.js
+++ Backend/Routes/route.js
@@ -3,8 +3,5 @@ const router = express.Router()
 import { getUserMessage } from '../controllers/User.js'
 
 router.post("/contact", getUserMessage)
-// router.get("/users", readDB)
-// router.delete("/users/:id", deleteUser)
-// router.put("/users/:id", updateUser)
 
 export default router;
--- Backend/controllers/User.js
+++ Backend/controllers/User.js
@@ -1,29 +1,5 @@
 import User from '../Model/UserModel.js'
 
-// export const readDB = async (req, res) => {
-//   try {
-//     const existingUsers = await User.find();
-
-//     if (existingUsers.length === 0) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "Users Not Found"
-//       });
-//     }
-
-//     res.status(200).json({
-//       success: true,
-//       users: existingUsers
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Error retrieving users",
-//       error: error.message
-//     });
-//   }
-// };
-
 
 export const getUserMessage = async (req, res) => {
   const { name, email, message } = req.body;
@@ -37,51 +13,6 @@ export const getUserMessage = async (req, res) => {
   }
   res.status(200).json({
     success: true,
-    message: "new user created successfully",
-    newUser
+    message: "message received successfully",
   })
 }
-
-// export const deleteUser = async (req, res) => {
-//   const { id } = req.params
-//   const deletedUser = await User.findByIdAndDelete(id)
-
-//   if (!deletedUser) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "unable to delete user"
-//     })
-//   }
-
-//   res.status(200).json({
-//     success: true,
-//     message: "user has been deleted successfully",
-//     deletedUser
-//   })
-// }
-
-// export const updateUser = async (req, res) => {
-//   try {
-//     const { id } = req.params
-//     const { name } = req.body;
-//     const updatedUser = await User.findByIdAndUpdate(id, { name }, { new: true })
-
-//     if (!updatedUser) {
-//       res.status(404).json({
-//         success: false,
-//         message: "no user found with the provided id"
-//       })
-//     }
-//     res.status(200).json({
-//       success: true,
-//       message: "user updated successfully",
-//       user: updatedUser
-//     })
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "data updation failed",
-//       error: error.message
-//     })
-//   }
-// }
